ce78e57c85f0e00f59246d462f788b19bd524ffc
Kelby Schick <kschick94@outlook.com>
2020-03-10 10:53:26 -0400

    testing 2

--- Regex-01/Grammar.ts
+++ Regex-01/Grammar.ts
@@ -24,6 +24,7 @@ export class Grammar {
             if (trial[i] == "") {
                 NonTerminals = true
             }
+
             else {
                 if (NonTerminals == false) { //TERMINALS SECTION
                     let current: string[] = trial[i].split("->");
